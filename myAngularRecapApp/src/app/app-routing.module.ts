30faa91f1fd8dba22447d27a9105e6278f850776
Monie Antoine - Full Stack Developer - Angular/React/NodeJS <monie.ant@gmail.com>
2022-09-21 16:34:36 +0200

    HomePage + Routing

--- myAngularRecapApp/src/app/app-routing.module.ts
+++ myAngularRecapApp/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+import { CompoComponent } from './demo/chapter2/2.1_composants/compo.component';
+
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+
+  // Path imbriqué :
+  {path:"demo", children : [
+    {path: "2.1_compo", component: CompoComponent}
+  ]}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- myAngularRecapApp/src/app/app.module.ts
+++ myAngularRecapApp/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+// Home :
+import { HomeComponent } from './home/home.component';
+
+// Shared :
+import { NavbarComponent } from './shared/navbar/navbar/navbar.component';
+
+// Chapter 2 :
+import { CompoComponent } from './demo/chapter2/2.1_composants/compo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CompoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
